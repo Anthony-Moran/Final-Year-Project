d22085b3e7c30521295acd305d4394e43661cbef
Anthony Moran <anthonymoran@Anthonys-MacBook-Pro.local>
2023-03-02 11:37:17 +0000

    Initialising basic connection from backend to front end

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,19 @@
+from flask import Flask, render_template, make_response
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Demo(Resource):
+    def get(self):
+        headers = {"Content-Type": "text/html"}
+        return make_response(render_template("index.html"), 200, headers)
+
+
 def main():
-    pass
+    api.add_resource(Demo, "/")
+    app.run(host='127.0.0.1', port='8000', debug=True)
 
 
 if __name__ == "__main__":
